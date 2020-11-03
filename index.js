174e86659b7b24cba3955027b71ed0a8a31c99d7
TheHund1 <matuszak.piotr@outlook.com>
2020-11-03 08:31:20 +0100

    heroku suuport może

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
+const PORT = process.env.port || 5000;
+
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
@@ -15,8 +17,11 @@ const server = new ApolloServer({
 mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('MongoDB Connected');
-        return server.listen({ port: 5000 });
+        return server.listen({ port: PORT });
     })
     .then((res) => {
         console.log(`Server running at ${res.url}`);
-    });
+    })
+    .catch(err => {
+        console.error(err)
+    })
