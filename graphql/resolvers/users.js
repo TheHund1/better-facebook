105b8a9f4d99725a0b242986c6ad87f8886095c9
TheHund1 <matuszak.piotr@outlook.com>
2020-11-17 10:37:06 +0100

    V2

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const{ SECRET_KEY } = require('../../config');
+const User = require('../../schemas/User');
+
+module.exports = {
+    Mutation: {
+        async register(_, { registerInput: { username, email, password, confirmPassword } }, context, info) {
+            // TODO vaildate user data
+            password = await bcrypt.hash(password, 12);
+
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString()
+            });
+
+            const res = await newUser.save();
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username
+            }, SECRET_KEY, {expiresIn: '1h'});
+            return{
+                ...res._doc,
+                id: res._id,
+                token
+            };
+        }
+    }
+}
